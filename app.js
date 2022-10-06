fac3fd0f25597bd55c2aec13a19b1f2fbc77e188
Saurabh S <saurabh.satralkar7@gmail.com>
2022-10-06 21:06:46 +0200

    EJS blog challenge v2: Styles added and challenges completed

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam";
-const aboutContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam.";
-const contactContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam.";
+const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam";
+const aboutContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam";
+const contactContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem accusantium explicabo, aliquam porro sequi suscipit neque fugiat consectetur delectus sed. Maxime porro iste reprehenderit deserunt cumque qui repellendus totam";
 
 const app = express();
 
@@ -15,4 +15,9 @@ app.use(express.static("public"));
 
 app.listen(3000, function(){
     console.log("Server started at port 3000");
+});
+
+// Display the content on home
+app.get("/",function(req,res){
+    res.render("home", {homeStartingContentFromHome: homeStartingContent});
 });
